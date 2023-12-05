5b1ba381278ce6fd922a63f7c7fc2ae0a6a1c579
Avairon <75504999+Avairon@users.noreply.github.com>
2023-12-05 17:03:54 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,28 +2,29 @@
 
 using namespace std;
 
-char* mystrtok(char* str, char space) {
+char* mystrtok(char *str, char space) {
     char* token;
 
     static int buff = 0;
     int i = buff;
 
-
-    do {
+    
+    do{
         token[i - buff] = str[i];
         i++;
-    } while (str[i] != space or str[i] != '\0');
+    }
+    while (str[i] != space);
 
     cout << buff << '\n';
     buff += i;
-
+    
     return token;
 }
 
-char* mystrcpy(char* dest, const char* donor) {
+char* mystrcpy(char* dest, const char* donor){
     int i = 0;
 
-    while (donor[i] != '\0') {
+    while(donor[i] != '\0'){
         dest[i] = donor[i];
         i++;
     }
@@ -33,16 +34,15 @@ char* mystrcpy(char* dest, const char* donor) {
     return dest;
 }
 
-char* mystrcat(char* dest, const char* donor) {
+char* mystrcat(char* dest, const char* donor){
     int i = 0;
     int i2 = 0;
 
-    while (dest[i] != '\0') {
-        ;
+    while(dest[i] != '\0'){;
         i++;
     }
-
-    while (donor[i2] != '\0') {
+    
+    while(donor[i2] != '\0'){
         dest[i + i2] = donor[i2];
         i2++;
     }
@@ -52,44 +52,44 @@ char* mystrcat(char* dest, const char* donor) {
     return dest;
 }
 
-int mystrlen(char* str) {
+int mystrlen(char *str) {
     int i = 0;
 
-    while (str[i] != '\0') i++;
+    while(str[i] != '\0') i++;
 
     return i;
 }
-int mystrcmp(char* str1, char* str2) {
+int mystrcmp(char *str1, char *str2) {
 
-    if (str1 == str2) return 0;
+    if(str1 == str2) return 0;
 
     int i = 0;
 
-    while (str1[i] != '\0' or str2[i] != '\0') {
+    while(str1[i] == '\0' or str2[i] == '\0'){
 
-        if (int(str1[i]) < int(str2[i])) return -1;
-        if (int(str1[i]) > int(str2[i])) return 1;
+        if(int(str1[i]) < int(str2[i])) return -1;
+        if(int(str1[i]) > int(str2[i])) return 1;
 
         i++;
+    }
+}
+
+int mystrncmp(char* str1, char* str2, size_t n){
+    
+    while(n){
+        str2++;
+        str1++;
+        if(*str1 < *str2) return -1;
+        if(*str1 > *str2) return 1;
+        n--;
     }
     return 0;
 }
 
-int mystrncmp(char* str1, char* str2, size_t n) {
-    int i = 0;
-
-    while (i < n) {
-        if (str1[i] == '\0' and str2[i] != '\0') return -1;
-        if (str1[i] != '\0' and str2[i] == '\0') return 1;
-        i++;
-    }
-    return 0;
-}
-
-char* mystrncpy(char* dest, const char* donor, size_t n) {
+char* mystrncpy(char* dest, const char* donor, size_t n){
     size_t i = 0;
 
-    while (donor[i] != '\0' and i < n) {
+    while(donor[i] != '\0' and i < n){
         dest[i] = donor[i];
         i++;
     }
@@ -99,16 +99,15 @@ char* mystrncpy(char* dest, const char* donor, size_t n) {
     return dest;
 }
 
-char* mystrncat(char* dest, const char* donor, size_t n) {
+char* mystrncat(char* dest, const char* donor, size_t n){
     int i = 0;
     int i2 = 0;
 
-    while (dest[i] != '\0') {
-        ;
+    while(dest[i] != '\0'){;
         i++;
     }
-
-    while (donor[i2] != '\0' and i2 < n) {
+    
+    while(donor[i2] != '\0' and i2 < n){
         dest[i + i2] = donor[i2];
         i2++;
     }
@@ -118,19 +117,19 @@ char* mystrncat(char* dest, const char* donor, size_t n) {
     return dest;
 }
 
-char* mystrstr(const char* target, char* donor) {
+char* mystrstr(char* target, char* donor){
     int i = 0;
     int i2 = 0;
 
     char* token;
 
-    while (donor[i] != target[i2]) {
-        if (donor[i] == '\0') return NULL;
+    while(donor[i] != target[i2]){
+        if(donor[i] == '\0') return NULL;
         i++;
     }
 
-    while (donor[i + i2] == target[i2]) {
-        if (donor[i + i2] == '\0' and token == NULL) return NULL;
+    while(donor[i + i2] == target[i2]){
+        if(donor[i + i2] == '\0' and token == NULL) return NULL;
         token[i2] = donor[i + i2];
         i2++;
     }
@@ -144,15 +143,15 @@ int main() {
     char test[256];
 
     cout << "start\nAll parameters in functions is (test, str, 5)\n\n";
-    //cout << mystrtok(str, ' ') << " :: strtok 1\n";
-    //cout << mystrtok(str, ' ') << " :: strtok 2\n";
+    cout << mystrtok(str, ' ') << " :: strtok 1\n";
+    cout << mystrtok(str, ' ') << " :: strtok 2\n";
     cout << mystrcpy(test, str) << " :: strcpy\n";
     cout << mystrncpy(test, str, 5) << " :: strncpy\n";
     cout << mystrcat(test, str) << " :: strcat\n";
     cout << mystrncat(test, str, 5) << " :: strncat\n";
     cout << mystrlen(test) << " :: strlen(test)\n";
     cout << mystrlen(str) << " :: strlen(str)\n";
-    cout << mystrcmp(test, str) << " :: strcmp\n";
+    //cout << mystrcmp(test, str) << " :: strcmp\n";
     cout << mystrncmp(test, str, 5) << " :: strncmp\n";
     cout << mystrstr("Hell", test) << " :: strstr\n";
 
